a80c6be7e35fdf48d121efa267d5a7f8ec4cc32d
tyronehou <tyronehou@wireless1x-155-41-111-84.bu.edu>
2015-04-29 16:59:13 -0400

    Added check win method

--- Player.java
+++ Player.java
@@ -8,18 +8,61 @@
 import java.util.*; 
 
 public class Player {
+    
+    private Random R = new Random(); 
+    
+    public int move(int[][] B) {
+        int m = 0;
+        for(int c = 0; c < 8; c++) {
+            m = move(B, c ,1);
+            if( m == 1 )
+                return m;
+        }
+        
+        return 5;
+    }
+    
+    private int move(int[][] B, int column, int player) {
+        
+    }
 
- private Random R = new Random(); 
- 
- public int move(int[][] B) {
-   int m = 0; 
-   do {
-     m = R.nextInt(8);
-   } while(B[0][m] != 0);
-   
-   return m; 
- }
-  
-  
-}
-
+    // check for win by player, 4 in a sequence
+    private static boolean checkWin(int player) {    // 1 = player, 10 = machine
+        
+        // check all horizontal rows
+        for(int i = 0; i < 8; ++i)
+            for(int j = 0; j < 5; ++j) {
+            if(slot[i][j] == player && slot[i][j+1] == player && slot[i][j+2] == player && slot[i][j+3] == player) {
+                return true;
+            }
+        }
+        
+        // check all vertical columns
+        for(int i = 0; i < 5; ++i)
+            for(int j = 0; j < 8; ++j) {
+            if(slot[i][j] == player && slot[i+1][j] == player && slot[i+2][j] == player && slot[i+3][j] == player) {
+                return true;
+            }
+        } 
+        
+        
+        // check all lower-left to upper-right diagonals
+        for(int i = 3; i < 8; ++i)
+            for(int j = 0; j < 5; ++j) {
+            
+            if(slot[i][j] == player && slot[i-1][j+1] == player && slot[i-2][j+2] == player && slot[i-3][j+3] == player) {
+                return true;
+            }  
+        }   
+        
+        // check all upper-left to lower-right diagonals
+        for(int i = 0; i < 5; ++i)
+            for(int j = 0; j < 5; ++j) {
+            if(slot[i][j] == player && slot[i+1][j+1] == player && slot[i+2][j+2] == player && slot[i+3][j+3] == player) {
+                return true;
+            }
+        }
+        
+        // no wins found
+        return false;  
+    }
